5b6f632f7161ee118cdaba09978f2944d7f04892
DerriKhan <101285294+DerriKhan@users.noreply.github.com>
2022-04-11 12:48:31 -0700

    added basic logic

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-conlsole.log('linked');
+
+document.getElementById("lightButton").addEventListener("click", lightJoke);
+function lightJoke() {
+var jokeLight = "https://v2.jokeapi.dev/joke/Any?safe-mode"
+
+fetch(jokeLight)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      console.log(data)
+    })
+}
+
+document.getElementById("darkButton").addEventListener("click", darkJoke);
+function darkJoke() {
+    var jokeDark = "https://v2.jokeapi.dev/joke/Any"
+fetch(jokeDark)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      console.log(data)
+    })
+}
+
+document.getElementById("inspireButton").addEventListener("click", inspire);
+function inspire() {
+    var inspiration = "https://api.goprogram.ai/inspiration"
+fetch(inspiration)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      console.log(data)
+    })
+}
+
+document.getElementById("despireButton").addEventListener("click", despire);
+function despire() {
+    var despiration = "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
+fetch(despiration)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      console.log(data)
+    })
+}
