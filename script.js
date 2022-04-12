8dc13a96b2e786d103d91090dff178d5851e73bc
iggy <allyson.nostrandd@gmail.com>
2022-04-12 13:22:16 -0700

    quotes are now displayed in the cards after button click

--- script.js
+++ script.js
@@ -1,77 +1,61 @@
+var darkCard = document.querySelector('#dark-text')
+var lightCard = document.querySelector('#light-text')
+var inspireCard = document.querySelector('#inspire-text')
+var despireCard = document.querySelector('#despire-text')
+
+
 document.getElementById("lightButton").addEventListener("click", lightJoke);
 function lightJoke() {
-var jokeLight = "https://v2.jokeapi.dev/joke/Any?safe-mode"
-var LJ = document.querySelector('#LJ')
-
-fetch(jokeLight)
+    var jokeLight = "https://v2.jokeapi.dev/joke/Any?safe-mode"  
+ fetch(jokeLight)
     .then(function (response) {
       return response.json()
     })
     .then(function (data) {
       console.log(data)
-
-      var quoteLocation = document.createElement('p')
-
-      quoteLocation.textContent=((data.setup) + (" ") + (data.delivery))
+      lightCard.textContent=((data.setup) + (" ") + (data.delivery))
       
-      LJ.append(quoteLocation)
     })
 }
 
 document.getElementById("darkButton").addEventListener("click", darkJoke);
 function darkJoke() {
     var jokeDark = "https://v2.jokeapi.dev/joke/Any?"
-    var LJ = document.querySelector('#DJ')
 fetch(jokeDark)
     .then(function (response) {
       return response.json()
     })
     .then(function (data) {
       console.log(data)
-      var quoteLocation = document.createElement('p')
-
-      quoteLocation.textContent=((data.setup) + (" ") + (data.delivery))
-      
-      DJ.append(quoteLocation)
+      darkCard.textContent=((data.setup) + (" ") + (data.delivery))
     })
 }
 
 document.getElementById("inspireButton").addEventListener("click", inspire);
 function inspire() {
     var inspiration = "https://api.goprogram.ai/inspiration"
-    var LJ = document.querySelector('#insp')
 fetch(inspiration)
     .then(function (response) {
-      console.log(data)
       return response.json()
     })
     .then(function (data) {
-      var quoteLocation = document.createElement('p')
-
-      quoteLocation.textContent=((data.setup) + (" ") + (data.delivery))
-      
-      insp.append(quoteLocation)
+      console.log(data)
+      inspireCard.textContent=((data.setup) + (" ") + (data.delivery))
     })
 }
 
 document.getElementById("despireButton").addEventListener("click", despire);
 function despire() {
     var despiration = "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
-    var LJ = document.querySelector('#desp')
 fetch(despiration)
     .then(function (response) {
-      console.log(data)
       return response.json()
     })
     .then(function (data) {
-      var quoteLocation = document.createElement('p')
-
-      quoteLocation.textContent=((data.setup) + (" ") + (data.delivery))
-      
-      desp.append(quoteLocation)
+      despireCard.textContent=((data.setup) + (" ") + (data.delivery))
     })
 }
-console.log('linked');
+
 /*modal funtion--------------------------------------------------------------------------------------*/
 $(document).ready(function(){
     $('.modal').modal();
